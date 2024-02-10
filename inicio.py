230d9db1585c2439707ef87375c77aed3e94cae0
Esteban Bejarano Caldas <jaimee.bejaranoc@autonoma.edu.co>
2024-02-09 20:49:08 -0500

    pasos del problema

---
+++ inicio.py
@@ -0,0 +1,9 @@
+#Se plantea realizar una interpolación para determinar las dimensiones de un mecanismo para un segmento rectilineo de 20 cm, con un rango aungular de 30 y 55 grados de giro de la manivela. 
+#Construir un paso a paso es esencial  para el desarrollo del problema, así que se hará de la siguiente forma: 
+# 1. Construir la tabla de datos en excel y cargarla a python.
+# 2. Identificar las librerias a utilizar.
+# 3. Analizar los datos cargados  a Python, realizando una descripción general del dataset.
+# 4. Aplicar al menos dos metodos de interpolacion acorde con los datos. 
+# 5. Evaluar  el rendimiento de cada uno de los metodos de interpolación.
+# 6. Realizar comparacion entre los diferentes metodos de  interpolación.
+# 7. Generar conclusiones
